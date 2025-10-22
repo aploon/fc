9a39c08865d77275ef32391bc7069dbd74ace030
Arnaud ADJOVI <arnaudadjovi274@gmail.com>
2025-10-22 18:30:43 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div style={{ padding: "2rem" }}>
+        <h1>
+          HeartConsultants LLC Portal
+        </h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/src/types/carbon-components-react.d.ts
@@ -0,0 +1,6 @@
+declare module 'carbon-components-react' {
+  const content: any;
+  export = content;
+}
+declare module 'carbon-components';
+declare module 'carbon-icons';
