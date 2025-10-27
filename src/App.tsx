5988dec7989f9d3ce2b98a031e87d8dca4313da0
Arnaud ADJOVI <arnaudadjovi274@gmail.com>
2025-10-27 16:15:22 +0100

    Refactor padding styles in App component for improved layout responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="app-container">
       <Header />
       <main className="app-main">
-        <div style={{ padding: "2rem" }}>
+        <div style={{ padding: "2rem", paddingLeft: "max(0px, (100% - 1600px) / 2)", paddingRight: "max(0px, (100% - 1600px) / 2)" }}>
           <h1>
             HeartConsultants LLC Portal
           </h1>
