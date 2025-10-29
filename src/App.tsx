7133289875ef8c40e2db4eb790de5b501ca25812
Arnaud ADJOVI <arnaudadjovi274@gmail.com>
2025-10-29 18:04:35 +0100

    Add tables and breadcrumbs to App component for enhanced data presentation and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { Table, TableHead, TableRow, TableHeader, TableBody, TableCell } from '@carbon/react'
+import { Breadcrumb, BreadcrumbItem } from '@carbon/react';
+import { OverflowMenu, OverflowMenuItem } from '@carbon/react';
+
+
 
 function App() {
   return (
@@ -9,6 +14,501 @@ function App() {
         <div className="app-container">
           <h1>Coming soon...</h1>
           <p>Explore IBM history and culture of putting technology to work in the real world.</p>
+
+          {/* somes components */}
+          {/* Tables */}
+          <Table
+            aria-label="sample table"
+            size="lg"
+          >
+            <TableHead>
+              <TableRow>
+                <TableHeader>
+                  Name
+                </TableHeader>
+                <TableHeader>
+                  Rule
+                </TableHeader>
+                <TableHeader>
+                  Status
+                </TableHeader>
+                <TableHeader>
+                  Other
+                </TableHeader>
+                <TableHeader>
+                  Example
+                </TableHeader>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 1
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Starting
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 2
+                </TableCell>
+                <TableCell>
+                  DNS delegation
+                </TableCell>
+                <TableCell>
+                  Active
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 3
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 4
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 5
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 6
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 7
+                </TableCell>
+                <TableCell>
+                Round robin             
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          {/* Bruncrumb */}
+          <Breadcrumb noTrailingSlash>
+            <BreadcrumbItem>
+              <a href="/#">
+                Breadcrumb 1
+              </a>
+            </BreadcrumbItem>
+            <BreadcrumbItem href="#">
+              Breadcrumb 2
+            </BreadcrumbItem>
+            <BreadcrumbItem data-floating-menu-container>
+              <OverflowMenu
+                align="bottom"
+                aria-label="Overflow menu in a breadcrumb"
+              >
+                <OverflowMenuItem itemText="Breadcrumb 3" />
+                <OverflowMenuItem itemText="Breadcrumb 4" />
+              </OverflowMenu>
+            </BreadcrumbItem>
+            <BreadcrumbItem href="#">
+              Breadcrumb 5
+            </BreadcrumbItem>
+            <BreadcrumbItem isCurrentPage>
+              Breadcrumb 6
+            </BreadcrumbItem>
+          </Breadcrumb>
+
+          {/* Tables */}
+          <Table
+            aria-label="sample table"
+            size="lg"
+          >
+            <TableHead>
+              <TableRow>
+                <TableHeader>
+                  Name
+                </TableHeader>
+                <TableHeader>
+                  Rule
+                </TableHeader>
+                <TableHeader>
+                  Status
+                </TableHeader>
+                <TableHeader>
+                  Other
+                </TableHeader>
+                <TableHeader>
+                  Example
+                </TableHeader>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 1
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Starting
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 2
+                </TableCell>
+                <TableCell>
+                  DNS delegation
+                </TableCell>
+                <TableCell>
+                  Active
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 3
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 4
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 5
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 6
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 7
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+
+          {/* Bruncrumb */}
+          <Breadcrumb noTrailingSlash>
+            <BreadcrumbItem>
+              <a href="/#">
+                Breadcrumb 1
+              </a>
+            </BreadcrumbItem>
+            <BreadcrumbItem href="#">
+              Breadcrumb 2
+            </BreadcrumbItem>
+            <BreadcrumbItem data-floating-menu-container>
+              <OverflowMenu
+                align="bottom"
+                aria-label="Overflow menu in a breadcrumb"
+              >
+                <OverflowMenuItem itemText="Breadcrumb 3" />
+                <OverflowMenuItem itemText="Breadcrumb 4" />
+              </OverflowMenu>
+            </BreadcrumbItem>
+            <BreadcrumbItem href="#">
+              Breadcrumb 5
+            </BreadcrumbItem>
+            <BreadcrumbItem isCurrentPage>
+              Breadcrumb 6
+            </BreadcrumbItem>
+          </Breadcrumb>
+          
+          {/* Tables */}
+          <Table
+            aria-label="sample table"
+            size="lg"
+          >
+            <TableHead>
+              <TableRow>
+                <TableHeader>
+                  Name
+                </TableHeader>
+                <TableHeader>
+                  Rule
+                </TableHeader>
+                <TableHeader>
+                  Status
+                </TableHeader>
+                <TableHeader>
+                  Other
+                </TableHeader>
+                <TableHeader>
+                  Example
+                </TableHeader>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 1
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Starting
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 2
+                </TableCell>
+                <TableCell>
+                  DNS delegation
+                </TableCell>
+                <TableCell>
+                  Active
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 3
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 4
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 5
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 6
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Load Balancer 7
+                </TableCell>
+                <TableCell>
+                  Round robin
+                </TableCell>
+                <TableCell>
+                  Disabled
+                </TableCell>
+                <TableCell>
+                  Test
+                </TableCell>
+                <TableCell>
+                  22
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
         </div>
       </main>
       <Footer />
